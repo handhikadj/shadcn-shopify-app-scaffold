8aa574d089676a8c632c5747450899a9985633d2
handhikadj <githubhandhikadj@gmail.com>
2024-05-14 11:26:05 +0700

    add shadcn stuff

---
+++ resources/js/Components/Shadcn/ui/table/TableRow.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { cn } from "@/lib/shadcn/utils";
+
+const props = defineProps({
+    class: { type: null, required: false },
+});
+</script>
+
+<template>
+    <tr
+        :class="
+            cn(
+                'border-b transition-colors',
+                props.class
+            )
+        "
+    >
+        <slot />
+    </tr>
+</template>
